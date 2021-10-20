c6aa6d879e81cc118ca41875b95f88db539a1cda
Jun Haneda <junkun601@gmail.com>
2021-10-20 11:15:10 +0900

    問題 list4py・reverse.py

---
+++ 1020/list4.py
@@ -0,0 +1,6 @@
+n=input("整数を入力してください")
+numbers = range(1,int(n)+1)
+total = sum(numbers)
+average = total/len(numbers)
+print(f"1～{n}までの合計：{total}")
+print(f"平均：{average}")
---
+++ 1020/str_reverse.py
@@ -0,0 +1,4 @@
+str1 = "abcdef"
+idx = len(str1)
+for i in reversed(str1):
+    print(i)
